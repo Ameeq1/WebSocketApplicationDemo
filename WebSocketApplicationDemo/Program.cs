4445177f60dd3d98d1da844ad27e843ef2dc10ad
ab <ab@AMEEQ-LAP>
2020-06-20 19:28:36 +0530

    teacher commit 2

--- WebSocketApplicationDemo/Program.cs
+++ WebSocketApplicationDemo/Program.cs
@@ -23,7 +23,7 @@ namespace WebSocketApplicationDemo
 
             //host.Run();
             // this is second test
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
